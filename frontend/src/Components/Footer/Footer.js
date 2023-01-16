a1eee50a6912839f86bb814eb4b0b80da724015c
손종효 <jbson1998@naver.com>
2023-01-16 21:08:26 +0900

    feat: 메인페이지제작
    메인페이지 상단바와 하단바를 구성한 상태입니다.
    
    Issue: S08P12B108-45
    
    Author: 손종효

---
+++ frontend/src/Components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return <div className="bottombar">하단바입니다.</div>;
+};
+
+export default Footer;
---
+++ frontend/src/Components/Rank/Rank.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Rank = () => {
+  return <div>랭크 페이지 입니다.</div>;
+};
+
+export default Rank;
