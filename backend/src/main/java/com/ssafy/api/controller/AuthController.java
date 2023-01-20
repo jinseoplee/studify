e4a69d1d9dfb40750f95905eeb944224315004d7
hmschlng <soulb9@naver.com>
2023-01-20 09:22:05 +0900

    refactor: 메일 인증 관련 로직 위치 이동
    리팩토링된 구조에 따라 메일 인증 관련 기능을 AuthController로 이동하였습니다.
    - api/controller/UserController -> api/controller/AuthController
    
    Issue:

--- backend/src/main/java/com/ssafy/api/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/api/controller/AuthController.java
@@ -1,21 +1,26 @@
 package com.ssafy.api.controller;
 
+import com.ssafy.api.request.UserAuthMailPostReq;
 import com.ssafy.api.request.UserLoginPostReq;
+import com.ssafy.api.request.UserRegisterPostReq;
 import com.ssafy.api.response.UserLoginPostRes;
 import com.ssafy.api.service.UserService;
+import com.ssafy.common.model.response.BaseResponseBody;
+import com.ssafy.config.mail.MailDispatcher;
+import com.ssafy.db.entity.Register;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/users/auth")
 public class AuthController {
 
     private final UserService userService;
+    private final MailDispatcher mailDispatcher;
 
     @PostMapping("/signin")
     public ResponseEntity<UserLoginPostRes> signin(@RequestBody UserLoginPostReq userLoginPostReq) {
@@ -23,4 +28,37 @@ public class AuthController {
         return ResponseEntity.status(200).body(userLoginPostRes);
     }
 
+    @PostMapping("/mail/register")
+    public ResponseEntity<? extends BaseResponseBody> sendAuthMail(
+            @RequestBody UserAuthMailPostReq req) throws Exception {
+
+        String certificationCode = mailDispatcher.getCertificationCode();
+        Long mailSentAt = System.currentTimeMillis();
+
+        /* 현재 시간 생성 */
+        String content = mailDispatcher.buildAuthMailContent(req.getName(), certificationCode, mailSentAt);
+
+        /* 메일 전송 */
+        mailDispatcher.sendMail(req.getEmail(), "Studify 회원가입 인증", content);
+
+        Register register = Register.builder()
+                .email(req.getEmail())
+                .password(req.getPassword())
+                .name(req.getName())
+                .certified(certificationCode)
+                .mailSentAt(mailSentAt)
+                .build();
+
+        userService.insertRegister(register);
+
+        /* 프론트에 응답 전송 */
+        return ResponseEntity.status(HttpStatus.OK).body(new BaseResponseBody(200, "Success"));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<? extends BaseResponseBody> signUp(@RequestBody UserRegisterPostReq registerPostReq) {
+        userService.createUser(userService.certificateRegister(registerPostReq));
+
+        return ResponseEntity.status(HttpStatus.OK).body(new BaseResponseBody(200, "Success"));
+    }
 }
