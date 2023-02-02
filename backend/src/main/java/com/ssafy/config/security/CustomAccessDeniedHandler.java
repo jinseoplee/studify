4bf8cfee2e944619703affa96aefbd7f3937c2ac
이진섭 <jjjp9966@gmail.com>
2023-02-02 16:43:35 +0900

    style: 주석 및 로깅 추가
    - api/controller/AuthController.java
    - config/security/CustomAccessDeniedHandler.java
    - config/security/CustomAuthenticationEntryPoint.java
    - config/security/JwtAuthenticationFilter.java
    - config/security/SecurityConfig.java
    
    Issue: S08P12B108-528

--- backend/src/main/java/com/ssafy/config/security/CustomAccessDeniedHandler.java
+++ backend/src/main/java/com/ssafy/config/security/CustomAccessDeniedHandler.java
@@ -1,5 +1,7 @@
 package com.ssafy.config.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.common.model.response.BaseResponseBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
@@ -11,12 +13,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * 권한이 없는 예외가 발생했을 경우 핸들링하는 클래스
+ */
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(CustomAccessDeniedHandler.class);
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        response.sendRedirect("/sign-api/exception");
+        ObjectMapper objectMapper = new ObjectMapper();
+        LOGGER.info("[handle] 접근 권한이 없습니다.");
+
+        BaseResponseBody baseResponseBody = new BaseResponseBody(403, "접근 권한이 없습니다.");
+
+        response.setStatus(403);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(objectMapper.writeValueAsString(baseResponseBody));
     }
 
 }
--- backend/src/main/java/com/ssafy/config/security/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,10 +12,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
- * 인증(authentication) 와 인가(authorization) 처리를 위한 스프링 시큐리티 설정 정의.
+ * 인증(authentication) 와 인가(authorization) 처리를 위한 스프링 시큐리티 설정 정의
  */
 @RequiredArgsConstructor
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
@@ -25,24 +27,22 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                // REST API는 UI를 사용하지 않으므로 기본설정을 비활성화
-                .httpBasic().disable()
-
-                // REST API는 csrf 보안이 필요 없으므로 비활성화
-                .csrf().disable()
-
-                // 토큰 기반 인증이므로 세션 사용 하지않음
+                .httpBasic().disable() // REST API는 UI를 사용하지 않으므로 기본 설정을 비활성화
+                .csrf().disable() // REST API는 csrf 보안이 필요 없으므로 비활성화
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT token 인증 방식은 세션이 필요 없으므로 비활성화
 
-                // 인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/v1/users/**" ).permitAll()
+                .antMatchers("/api/v1/users/**").permitAll()
 
-                // usernamePasswordAuthenticationFilter 앞에 jwtAuthenticationFilter 추가
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and()
+                .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
+
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
