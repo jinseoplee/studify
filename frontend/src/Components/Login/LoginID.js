e16647ef229ca1bd3eca43dc84288395502425ed
손종효 <jbson1998@naver.com>
2023-02-04 21:14:38 +0900

    fix:CSS변경
    로그인 페이지 CSS변경
    Issue: S08P12B108-405

--- frontend/src/Components/Login/LoginID.js
+++ frontend/src/Components/Login/LoginID.js
@@ -2,24 +2,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LoginCard from "./LoginCard";
-import LoginidStyle from "../../Style/Login/LoginID.module.css";
+import LoginStyle from "../../Style/Login/Login.module.css";
 
 const LoginID = (props) => {
   return (
     <LoginCard>
-      <form
-        onSubmit={props.handleSubmit}
-        className={LoginidStyle.loginidIdform}
-      >
-        <h2 className={LoginidStyle.loginidText}>로그인</h2>
-        <label className={LoginidStyle.loginidEmailform}>
+      <form onSubmit={props.handleSubmit} className={LoginStyle.loginForm}>
+        <h2 className={LoginStyle.loginText}>로그인(이메일)</h2>
+        <label className={LoginStyle.loginEmailform}>
           <input
             type="text"
             id="email"
             value={props.email}
             name="email"
             onChange={props.handleChange}
-            className={LoginidStyle.loginidTextform}
+            className={LoginStyle.loginTextform}
             placeholder="test@test.com"
           />
         </label>
@@ -29,18 +26,18 @@ const LoginID = (props) => {
             id="saveid"
             onChange={props.togglecheck}
             checked={props.isRemember}
-            className={LoginidStyle.loginidCheckboxform}
+            className={LoginStyle.loginCheckboxform}
           />
           <label htmlFor="saveid">아이디저장</label>
         </fieldset>
-        <div className={LoginidStyle.loginidBottom}>
+        <div className={LoginStyle.loginBottom}>
           아이디가 존재하지 않으신가요? &nbsp;
-          <Link to="/user/signup" className={LoginidStyle.loginidText_link}>
+          <Link to="/user/signup" className={LoginStyle.loginText_link}>
             회원가입
           </Link>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <button type="submit" className={LoginidStyle.loginidBtnform}>
-            다음
+          <button type="submit" className={LoginStyle.loginBtnform}>
+            >>>
           </button>
         </div>
       </form>
--- frontend/src/Components/Login/LoginPW.js
+++ frontend/src/Components/Login/LoginPW.js
@@ -2,7 +2,8 @@
 import React from "react";
 import ModalLogin from "../UI/ModalLogin";
 
-// import "../../Style/Login/LoginPW.css";
+import LoginStyle from "../../Style/Login/Login.module.css";
+import LoginCard from "./LoginCard";
 
 const LoginPW = ({
   handleSubmit,
@@ -14,21 +15,38 @@ const LoginPW = ({
   closeModal,
 }) => {
   return (
-    <React.Fragment>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="password"
-          name="email"
-          value={password}
-          onChange={handleChange}
-        />
-        <button type="submit" onClick={check}>
-          다음
-        </button>
+    <LoginCard>
+      <form onSubmit={handleSubmit} className={LoginStyle.loginForm}>
+        <h2 className={LoginStyle.loginText}>로그인(비밀번호)</h2>
+        <label className={LoginStyle.loginEmailform}>
+          <input
+            type="password"
+            name="email"
+            value={password}
+            onChange={handleChange}
+            className={LoginStyle.loginTextform}
+          />
+        </label>
+        <div onClick={findPw} className={LoginStyle.loginBottom}>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;비밀번호
+          잊어버리셨나요? &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          {modalOpen && (
+            <ModalLogin
+              setModalOpen={closeModal}
+              modlOpen={modalOpen}
+            ></ModalLogin>
+          )}
+          <button
+            type="submit"
+            onClick={check}
+            className={LoginStyle.loginBtnform}
+          >
+            >>>
+          </button>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
       </form>
-      <div onClick={findPw}>비밀번호 잊어버리셨나요?</div>
-      {modalOpen && <ModalLogin setModalOpen={closeModal}></ModalLogin>}
-    </React.Fragment>
+    </LoginCard>
   );
 };
 
