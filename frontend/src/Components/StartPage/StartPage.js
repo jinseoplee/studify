d0bd7b3b39d1e3b8b4c58e979fede9fcf2ca8b50
김영록 <yr7256@gmail.com>
2023-02-09 03:30:03 +0900

    feat: 메인 페이지 구현 및 css
    - src/Components/StartPage/StartPage.js
    - src/Style/Startpage/StartPage.module.css
    
    Issue: S08P12B108-717

--- frontend/src/Components/StartPage/StartPage.js
+++ frontend/src/Components/StartPage/StartPage.js
@@ -26,16 +26,27 @@ const StartPage = () => {
         />
       </div>
       <div className={StartPageStyle.StartPageSignup}>
-        <div className={StartPageStyle.StartPageContent}>
+        <div className={StartPageStyle.StartPageSignupContent}>
           <p>나만의 스터디 관리를 원하시나요?</p>
           <button className={StartPageStyle.startpageBtn} onClick={goSignup}>
-            회원가입하러 가기
+            시작하기
           </button>
         </div>
         <img data-aos="fade-left" src={Emoji} alt="Emoji" className={StartPageStyle.StartPageImg}/>
       </div>
-      <div>
-        <p>hi</p>
+      <div className={StartPageStyle.StartPageStudyroom}>
+        <div className={StartPageStyle.StartPageStudyroomContent}>
+          <h1>나만의 스터디를 만들고 화상 스터디 방을 만들 수 있습니다</h1>
+          <p>스터디에 대한 설명 하나둘셋</p>
+        </div>
+        <img data-aos="fade-right" src={Emoji} alt="Studyroom" className={StartPageStyle.StartPage}/>
+      </div>
+      <div className={StartPageStyle.StartPageMyStudy}>
+        <div className={StartPageStyle.StartPageMyStudyContent}>
+          <h1>스터디와 자신의 공부를 기록 관리할 수 있습니다</h1>
+          <p>자기 공부 관리, 스터디 시간 기록, 동기부여 등</p>
+        </div>
+        <img data-aos="fade-left" src={Emoji} alt="Studyroom" className={StartPageStyle.StartPage}/>
       </div>
     </React.Fragment>
   );
