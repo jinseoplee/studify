edad756a60ca13156604e954ea25b4cbe525bee3
yr7256 <yr7256@gmail.com>
2023-02-08 12:33:00 +0900

    feat: 랭킹 pagination, css
    랭킹 pagination과 css 작업을 했습니다.
    
    Issue: S08P12B108-694

--- frontend/src/Components/Rank/RankPodium.js
+++ frontend/src/Components/Rank/RankPodium.js
@@ -5,19 +5,19 @@ const RankPodium = (props) => {
   const { data } = props;
   return (
     <div className={RankStyle.RankPodiumContainer}>
-      <div className={RankStyle.RankPodiumDetailContainer}>
+      <div className={RankStyle.RankPodiumDetail}>
         <div className={RankStyle.RankSecond} />
         <p>
           {data[1].name} {data[1].time}
         </p>
       </div>
-      <div className={RankStyle.RankPodiumDetailContainer}>
+      <div className={RankStyle.RankPodiumDetail}>
         <div className={RankStyle.RankFirst} />
         <p>
           {data[0].name} {data[0].time}
         </p>
       </div>
-      <div className={RankStyle.RankPodiumDetailContainer}>
+      <div className={RankStyle.RankPodiumDetail}>
         <div className={RankStyle.RankThird} />
         <p>
           {data[2].name} {data[2].time}
