ac27779eea5dea51acdebb508206f8efec05d497
hmschlng <soulb9@naver.com>
2023-02-13 21:49:10 +0900

    refactor: 머지되지 않은 충돌 부분 해결
    해결되지 않은 충돌 사항들을 정리했습니다.
    
    Issue:

--- frontend/src/pages/MainStudy/StudyDetails.js
+++ frontend/src/pages/MainStudy/StudyDetails.js
@@ -1,12 +1,9 @@
 import React from "react";
 import Topbar from "../../Components/Topbar/Topbar";
 import StudyDetail from "../../Components/MainStudy/StudyDetail";
-<<<<<<< HEAD
 import Footer from "../../Components/Footer/Footer"
-=======
 import TokenCheck from "../../Components/TokenCheck/TokenCheck";
 
->>>>>>> 132e22be29918e3d9e3daac1b37d7507be408da9
 const StudyDetails = () => {
   return (
     <>
