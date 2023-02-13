f8496042114f776e215e6a7df59b5cb35ae92a84
이방환 <soulb9@naver.com>
2023-02-13 12:12:21 +0900

    refactor: Swagger-ui 진입을 위해 필터 설정 변경
    Reverse proxy 환경에서 Swagger-ui로 진입하기 위해 antMatchers에 "/swagger-ui/**" 경로를 추가했습니다.
    - config/security/SecurityConfig.java
    
    Issue:

--- backend/src/main/java/com/ssafy/config/security/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/config/security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/v1/users/**").permitAll()
+                .antMatchers("/api/v1/users/**", "/swagger-ui/**").permitAll()
                 .antMatchers("/api/v1/studies/**").authenticated()
 
                 .and()
