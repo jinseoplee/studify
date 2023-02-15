5ec854a7a1b61c331e5fab7834dc0fb4db45b09e
하세진 <tpwls86@naver.com>
2023-02-15 22:40:58 +0900

    feat:가입하기 버튼 구현
    가입하기 버튼 완료했습니다

--- frontend/src/Components/MainStudy/StudyDetail.js
+++ frontend/src/Components/MainStudy/StudyDetail.js
@@ -11,9 +11,11 @@ const StudyDetail = () => {
   const navigate = useNavigate();
   // const outletData = { studyData: studydata };
   const studyId = useSelector((state) => state.selectday.studyNum);
+  const checkMystudy = useSelector((state) => state.selectday.mystudy);
   const userToken = useSelector((state) => state.token.accesstoken);
   const [studydata, setStudyData] = useState("");
-  let studyUserCheck = false;
+  const [studyUser, setStudyUser] = useState([]);
+  const [check, setCheck] = useState(false);
   const [dummyimg, setImg] = useState(
     "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/A1Z/image/luE0JoNHfcq1dK6y1_6lAvvTqDI.jpg"
   );
@@ -25,43 +27,16 @@ const StudyDetail = () => {
   };
 
   //유저 확인
-  const userCheckHandler = () => {
-    studydata &&
-      studydata.users.some((el) => {
-        if (el !== userEmail) {
-          console.log("안맞아요");
-          studyUserCheck = true;
-        }
-        return studyUserCheck === true;
-      });
-  };
-
   //오픈 비두 참가
   let url = "/usercheck";
   const joinSession = () => {
     window.open(url, "_blank", "noopener noreferrer");
   };
-  
-  // const getStudydata = async () => {
-  //   try{
-  //     const response = await axios
-  //     .get(`/api/v1/studies/${studyId}`, {
-  //       headers: {
-  //         "X-Auth-Token": `${userToken}`,
-  //       },
-  //     })
-  //     response.then((res) => {
-  //       console.log(res);
-  //       setStudyData(res.data.content);
-  //     })
-  //   } catch(err) {
-  //     console.log(err)
-  //   }
-  // }
 
-  // useEffect(() => {
-  //   getStudydata();
-  // }, []);
+  // const userCheckhandler = (studyUser, userEmail) => {
+  //   console.log("ok");
+  //   console.log(studyUser.includes(userEmail));
+  // };
 
   const studyDataHandler = async () => {
     try {
@@ -70,17 +45,15 @@ const StudyDetail = () => {
           "X-Auth-Token": `${userToken}`,
         },
       });
-      console.log(res);
-      localStorage.setItem("studyId", studyId);
       setStudyData(res.data.content);
+      setStudyUser(res.data.content.userList);
+      console.log(res);
     } catch (err) {
       console.log(err);
     }
   };
-
   useEffect(() => {
     studyDataHandler();
-    userCheckHandler();
   }, []);
 
   const outStudyHandler = () => {
@@ -124,10 +97,7 @@ const StudyDetail = () => {
       <div className={StudyStyle.StudyDetailContainer}>
         <div style={imgstyle}>
           <div className={StudyStyle.StudyDetailback}>
-            <p className={StudyStyle.StudyDetailName}>
-              {studydata.title}
-              {studyUserCheck}
-            </p>
+            <p className={StudyStyle.StudyDetailName}>{studydata.title}</p>
             <div className={StudyStyle.StudyDetailButtons}>
               {studydata.users &&
                 studydata.users.map((el) => (
@@ -142,8 +112,7 @@ const StudyDetail = () => {
                     )}
                   </div>
                 ))}
-
-              {studyUserCheck === true && (
+              {!studyUser.includes(userEmail) && (
                 <button
                   className={StudyStyle.StudyBtn}
                   onClick={joinStudyHandler}
